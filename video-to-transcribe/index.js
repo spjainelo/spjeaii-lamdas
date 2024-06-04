54fcd02cd7d9cc4c5d97a8df88031c49092b2e5e
AtharMTurab <athar.turab@enfintechnologies.com>
2024-06-04 12:51:44 +0530

    [FEATURE] Video To Transcription Lamda Funcation created

--- video-to-transcribe/index.js
+++ video-to-transcribe/index.js
@@ -1,13 +1,66 @@
-exports.handler = async function (event, context) {
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  TranscribeClient,
+  StartTranscriptionJobCommand,
+} = require("@aws-sdk/client-transcribe");
+const path = require("path");
+
+const NodeEnv = process.env.NODE_ENV;
+const Region = process.env.AWS_DEFAULT_REGION || "us-east-1";
+const LanguageCode = "en-US";
+const MediaFormat = "mp4";
+
+const client = new S3Client({});
+const transcribeClient = new TranscribeClient({ region: Region });
+
+const getObjectFromS3 = async ({ Bucket, Key }) => {
+  let directoryPath = path.dirname(Key);
+  directoryPath = directoryPath + "/params.json";
+
+  const command = new GetObjectCommand({ Bucket, Key: directoryPath });
   try {
-    console.log("INDEX EXPORT HANDLER", event);
-  } catch (error) {
-    console.log(error);
+    const response = await client.send(command);
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    return await response.Body.transformToString();
+  } catch (err) {
+    console.error(err);
   }
+};
 
+exports.handler = async function (event, context) {
+  // console.log("INDEX EXPORT HANDLER", event);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
+  try {
+    const eRecord = event.Records && event.Records[0],
+      inputBucket = eRecord.s3.bucket.name,
+      key = eRecord.s3.object.key;
+
+    const s3Result = await getObjectFromS3({ Bucket: inputBucket, Key: key });
+    const s3Data = JSON.parse(s3Result);
+
+    const TranscriptionJobName = s3Data.questionId + "_" + Date.now();
+    const MediaFileUri = `https://${inputBucket}.s3.amazonaws.com/${key}`;
+    const OutputKey = `${NodeEnv}/output-json/${TranscriptionJobName}.json`;
+
+    const params = {
+      TranscriptionJobName,
+      LanguageCode, // For example, 'en-US'
+      MediaFormat, // For example, 'mp4'
+      Media: {
+        MediaFileUri, // For example, "https://transcribe-demo.s3-REGION.amazonaws.com/hello_world.wav"
+      },
+      OutputKey,
+      OutputBucketName: inputBucket,
+    };
+
+    const data = await transcribeClient.send(
+      new StartTranscriptionJobCommand(params)
+    );
+    if(data) console.log('transcribed sucessfully');
+    return { statusCode: 200, body: data };
+  } catch (err) {
+    console.log(error);
+
+    return { statusCode: 400, body: err };
+  }
 };
--- video-to-transcribe/test.js
+++ video-to-transcribe/test.js
@@ -1,46 +1,51 @@
+require('dotenv').config();
+
 const event = {
     "Records": [
-      {
-        "eventVersion": "2.1",
-        "eventSource": "aws:s3",
-        "awsRegion": "us-west-2",
-        "eventTime": "1970-01-01T00:00:00.000Z",
-        "eventName": "ObjectCreated:Put",
-        "userIdentity": {
-          "principalId": "AIDAJDPLRKLG7UEXAMPLE"
-        },
-        "requestParameters": {
-          "sourceIPAddress": "127.0.0.1"
-        },
-        "responseElements": {
-          "x-amz-request-id": "C3D13FE58DE4C810",
-          "x-amz-id-2": "FMyUVURIY8/IgAtTv8xRjskZQpcIZ9KG4V5Wp6S7S/JRWeUWerMUE5JgHvANOjpD"
-        },
-        "s3": {
-          "s3SchemaVersion": "1.0",
-          "configurationId": "testConfigRule",
-          "bucket": {
-            "name": "mybucket",
-            "ownerIdentity": {
-              "principalId": "A3NL1KOZZKExample"
+        {
+            "eventVersion": "2.1",
+            "eventSource": "aws:s3",
+            "awsRegion": "us-east-1",
+            "eventTime": "2024-06-04T05:20:52.576Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {
+                "principalId": "AWS:AIDAVDHVW3PS5MM4VAROO"
             },
-            "arn": "arn:aws:s3:::mybucket"
-          },
-          "object": {
-            "key": "/uploads/xyz.mykademy.com/items/videos/video20-10-2020-10-36-7/str72000038.txt",
-            "size": 1024,
-            "eTag": "d41d8cd98f00b204e9800998ecf8427e",
-            "versionId": "096fKKXTRTtl3on89fVO.nfljtsv6qko",
-            "sequencer": "0055AED6DCD90281E5"
-          }
+            "requestParameters": {
+                "sourceIPAddress": "103.121.26.34"
+            },
+            "responseElements": {
+                "x-amz-request-id": "9RFZSGJV4AJ72NSQ",
+                "x-amz-id-2": "flc0R+pVMbhSp7f2GXNEJNPBT4NVUSn3MsrWpDr5GzlRWyepfU764zl4VvS8lbEEvMjpyrKfMXgKknXYZDss2ZvtCbfa85FI"
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "27222762-a461-4ddd-9fc2-f862e9f34f5e",
+                "bucket": {
+                    "name": "spjeaii-assets",
+                    "ownerIdentity": {
+                        "principalId": "ABA1KYRDC3ZPH"
+                    },
+                    "arn": "arn:aws:s3:::spjeaii-assets"
+                },
+                "object": {
+                    "key": "development/recordings/a511b33ec922445b9294d17070c644ed/demo.mp4",
+                    "size": 189070,
+                    "eTag": "b229d3a99565f5b9d3cc31524539c9ca",
+                    "sequencer": "00665EA434767B0F86"
+                }
+            }
         }
-      }
     ]
-  }
-  
-  process.env.ENDPOINT = 'http://localhost:8000';
-  process.env.BANDWIDTH_TABLE = 'Bandwidth';
-  
-  const { handler } = require('./index');
-  
-  handler(event);
+}
+
+// Import ENV's
+process.env.AWS_BUCKET_URL;
+process.env.NODE_ENV;
+process.env.AWS_BUCKET;
+process.env.AWS_DEFAULT_REGION;
+process.env.LANGUAGE_CODE;
+
+const { handler } = require('./index');
+
+handler(event);
